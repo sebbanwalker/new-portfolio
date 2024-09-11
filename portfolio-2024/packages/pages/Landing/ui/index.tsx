bb584fd8547818c269ce00da0bc624cad3d041b4
sebbanwalker <64078193+sebbanwalker@users.noreply.github.com>
2024-09-11 19:29:10 +0200

    landing page banner titles

--- portfolio-2024/packages/pages/Landing/ui/index.tsx
+++ portfolio-2024/packages/pages/Landing/ui/index.tsx
@@ -3,7 +3,12 @@ import "./style.scss";
 export const LandingPage = () => {
 	return (
 		<section className="landing">
-			<section className="landing__banner"></section>
+			<section className="landing__banner">
+				<section className="landing__title">
+					<h1 className="landing__title--sebastian">Sebastian</h1>
+					<h1 className="landing__title--walker">Walker</h1>
+				</section>
+			</section>
 		</section>
 	);
 };
