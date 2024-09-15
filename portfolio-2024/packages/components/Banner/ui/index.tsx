c0a13a3a71f290778a583937a42e912bd6e36d47
sebbanwalker <64078193+sebbanwalker@users.noreply.github.com>
2024-09-15 04:39:45 +0200

    factored banner component

--- portfolio-2024/packages/components/Banner/ui/index.tsx
+++ portfolio-2024/packages/components/Banner/ui/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-
-type Props = {};
-
-const index = (props: Props) => {
-	return <div>index</div>;
+export const Banner = () => {
+	return (
+		<section className="landing__banner">
+			<section className="landing__title">
+				<h1 className="landing__title--sebastian">Sebastian</h1>
+				<h1 className="landing__title--walker">Walker</h1>
+			</section>
+			<section className="landing__portrait"></section>
+		</section>
+	);
 };
-
-export default index;
--- portfolio-2024/packages/pages/Landing/ui/index.tsx
+++ portfolio-2024/packages/pages/Landing/ui/index.tsx
@@ -1,15 +1,10 @@
 import "./style.scss";
+import { Banner } from "@sebban/banner";
 
 export const LandingPage = () => {
 	return (
 		<section className="landing">
-			<section className="landing__banner">
-				<section className="landing__title">
-					<h1 className="landing__title--sebastian">Sebastian</h1>
-					<h1 className="landing__title--walker">Walker</h1>
-				</section>
-				<section className="landing__portrait"></section>
-			</section>
+			<Banner />
 			<section className="landing__main-stack"></section>
 			<section className="landing__other-tech"></section>
 		</section>
