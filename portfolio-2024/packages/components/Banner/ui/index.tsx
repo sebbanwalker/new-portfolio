3eb25256bfd65b1ae6f354301ef1e20664bf9a4d
sebbanwalker <64078193+sebbanwalker@users.noreply.github.com>
2024-09-19 13:17:37 +0200

    typography on banner fixed

--- portfolio-2024/packages/components/Banner/ui/index.tsx
+++ portfolio-2024/packages/components/Banner/ui/index.tsx
@@ -5,8 +5,8 @@ export const Banner = () => {
 	return (
 		<section className="banner">
 			<section className="banner__title">
-				<h1 className="banner__title--sebastian">Sebastian</h1>
-				<h1 className="banner__title--walker">Walker</h1>
+				<h1 className="banner__title--sebastian">SEBASTIAN</h1>
+				<h1 className="banner__title--walker">WALKER</h1>
 			</section>
 			<section className="banner__portrait">
 				<img src={portrait} className="banner__portrait--photo"></img>
