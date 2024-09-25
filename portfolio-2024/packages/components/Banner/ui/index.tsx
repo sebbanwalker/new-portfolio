dbabe7291bc3af430975d819de05cf1f1cb2561d
sebbanwalker <64078193+sebbanwalker@users.noreply.github.com>
2024-09-25 17:07:40 +0200

    added links to banner

--- portfolio-2024/packages/components/Banner/ui/index.tsx
+++ portfolio-2024/packages/components/Banner/ui/index.tsx
@@ -1,5 +1,8 @@
 import "./style.scss";
 import portrait from "/portrait.png";
+import github from "/github.svg";
+import linkedin from "/linkedin.svg";
+import leetcode from "/leetcode.svg";
 
 export const Banner = () => {
 	return (
@@ -9,6 +12,18 @@ export const Banner = () => {
 					<h1 className="banner__title--sebastian">SEBASTIAN</h1>
 					<h1 className="banner__title--walker">WALKER</h1>
 				</div>
+				<p>Impassioned Frontend Developer based in Stockholm, Sweden.</p>
+				<section className="banner__links">
+					<a href="https://github.com/sebbanwalker">
+						<img src={github} className="banner__link--image"></img>
+					</a>
+					<a href="https://linkedin.com/in/sebbanwalker">
+						<img src={linkedin} className="banner__link--image"></img>
+					</a>
+					<a href="https://leetcode.com/sebbanwalker">
+						<img src={leetcode} className="banner__link--image"></img>
+					</a>
+				</section>
 			</section>
 			<section className="banner__portrait">
 				<img src={portrait} className="banner__portrait--photo"></img>
