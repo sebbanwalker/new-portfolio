87a44bf46012fed52f9d2d389c0a25144deb4052
sebbanwalker <64078193+sebbanwalker@users.noreply.github.com>
2024-09-25 16:35:02 +0200

    contained title

--- portfolio-2024/packages/components/Banner/ui/index.tsx
+++ portfolio-2024/packages/components/Banner/ui/index.tsx
@@ -5,8 +5,10 @@ export const Banner = () => {
 	return (
 		<section className="banner">
 			<section className="banner__title">
-				<h1 className="banner__title--sebastian">SEBASTIAN</h1>
-				<h1 className="banner__title--walker">WALKER</h1>
+				<div className="banner__title--container">
+					<h1 className="banner__title--sebastian">SEBASTIAN</h1>
+					<h1 className="banner__title--walker">WALKER</h1>
+				</div>
 			</section>
 			<section className="banner__portrait">
 				<img src={portrait} className="banner__portrait--photo"></img>
