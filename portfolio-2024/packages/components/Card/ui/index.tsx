5a5402ab7c30409cd6e5f31402bc6d42fcaaa301
sebbanwalker <64078193+sebbanwalker@users.noreply.github.com>
2024-10-07 16:07:33 +0200

    created stylesheets and added more functionality to card component

--- portfolio-2024/packages/components/Card/ui/index.tsx
+++ portfolio-2024/packages/components/Card/ui/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { images } from "@sebban/images";
+import "./style.scss";
 
 interface link {
 	text: string;
@@ -31,10 +32,33 @@ export const Card = (props: cardProps) => {
 	return (
 		<section className="card">
 			<section className="card__container">
-				<img src={props.image} alt={props.imageAlt} />
+				<img src={props.image} alt={props.imageAlt} className="card__image" />
 				{props.icons &&
 					props.icons.map((icon, index: number) => (
-						<img src={icon.icon} alt={icon.iconName} />
+						<img src={icon.icon} alt={icon.iconName} key={index} />
+					))}
+				{props.score &&
+					props.score.map((score, index: number) => (
+						<section key={index}>
+							<p className="card__score--text">{score.text}</p>
+							{score.highlight && (
+								<img
+									src={score.highlight}
+									className="card__score--text--highlight"></img>
+							)}
+							{score.icon && (
+								<img className="card__score--img">{score.icon} </img>
+							)}
+						</section>
+					))}
+				{props.link &&
+					props.link.map((link, index: number) => (
+						<section key={index}>
+							{link.text && <p className="card__link--text">{link.text}</p>}
+							{link.icon && (
+								<img src={link.icon} className="card__link--image"></img>
+							)}
+						</section>
 					))}
 			</section>
 		</section>
